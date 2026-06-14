18225ed51b1256b978d17727187d75cbb1e817e0
Shyam <meherkarshyam@gmail.com>
2026-06-14 23:15:48 +0530

    fix: simplify motion-client import, add framer-motion dependency

--- src/components/shared/motion-client.tsx
+++ src/components/shared/motion-client.tsx
@@ -1,3 +1,2 @@
 "use client";
-import { div, p, span, a, button, section, h1, h2, h3, h4, h5, h6, img, ul, li } from "motion/react-client";
-export const m = { div, p, span, a, button, section, h1, h2, h3, h4, h5, h6, img, ul, li };
+export { m } from "motion/react";
